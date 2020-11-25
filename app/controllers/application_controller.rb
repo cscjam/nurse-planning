7b508b85c7bbe5e04fe0f28a1d9e06460032b698
Charlotte Martineau <charlotte_martineau@hotmail.fr>
2020-11-25 11:14:30 +0100

    Refacto de l'agenda

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def days
+    @days = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi"]
+  end
 end
