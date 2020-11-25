4b7a2d08d8b4f18b5bdc5dae31e5723290c542ed
CharlotteMartineau <70255612+CharlotteMartineau@users.noreply.github.com>
2020-11-25 14:58:33 +0100

    Update application_record.rb

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,9 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  @@DAYS = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
-
-  def self.get_days
-    DAYS
-  end
 end
