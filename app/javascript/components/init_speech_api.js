ffe8fcc556ed83867dfd49bbac1625b98e5042d7
Florent BRAURE <florent.braure@gmail.com>
2020-11-25 10:18:20 +0100

    add speech js

---
+++ app/javascript/components/init_speech_api.js
@@ -0,0 +1,28 @@
+export const initSpeechApi = () => {
+  const speechElt = document.getElementById("speech");
+  const keepPushBtn = document.getElementById("keep-push-record");
+
+  if(speechElt && keepPushBtn) {
+    const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+    recognition.lang = 'fr-FR';
+    // Contrôle si la reconnaissance est continue, ou retourne seulement un seul résultat.
+    recognition.continuous = true;
+    // Contrôle si les résultats intermédiaires doivent être retournés (true) ou pas (false.)
+    // Les résultats intermédiaires sont des résultats qui ne sont pas encore définitifs
+    recognition.interimResults = false;
+    // Règle le nombre maximum de SpeechRecognitionAlternative (d'alternatives) fourni par résultat.
+    recognition.maxAlternatives = 1;
+
+    recognition.onresult = function(event) {
+      speechElt.innerText = event.results[0][0].transcript;
+    };
+    keepPushBtn.addEventListener("mousedown", event => {
+      speechElt.innerText = "";
+      recognition.start();
+    });
+    keepPushBtn.addEventListener("mouseup", event => {
+      recognition.stop();
+    });
+  }
+};
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-
   def get_full_name
     "#{first_name.capitalize} #{last_name.upcase}"
   end
