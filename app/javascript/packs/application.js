bbafc0dd409917e775f1431f5a44f037adac67bb
Florent BRAURE <florent.braure@gmail.com>
2020-11-27 22:15:04 +0100

    Add Text Synthetisor

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,11 @@ import "bootstrap";
 
 // Internal imports, e.g:
 import { initMinuteCreation } from '../components/init_minute_creation.js';
+import { initSpeechApi } from "../components/init_speech_api.js";
 import { flatPickr } from "../plugins/flatpickr"
 
 document.addEventListener('turbolinks:load', () => {
+  initSpeechApi();
   initMinuteCreation();
   flatPickr();
 });
