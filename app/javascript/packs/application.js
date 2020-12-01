7876bce3a004c993bfa1c33f33c8ed89cff51ef5
Charlotte Martineau <charlotte_martineau@hotmail.fr>
2020-12-01 10:17:15 +0100

    Création modal, ne fonctionne que pour une image

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,3 +20,25 @@ document.addEventListener('turbolinks:load', () => {
   flatPickr();
   toggleDone();
 });
+
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+const img = document.querySelectorAll("myImg");
+const modalImg = document.getElementById("img01");
+img.addEventListener("click", (event) => {
+
+}
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+// }
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
