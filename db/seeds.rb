b18e6fe8a9aeba21a19cf45e9468d5d721277237
Julie Gabach <julie.gabach@gmail.com>
2020-12-02 13:13:23 +0100

    revue du seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,72 +16,87 @@ jackie = User.new(
 file = URI.open('https://streamondemandathome.com/wp-content/uploads/2016/01/NurseJackie.jpg')
 jackie.avatar.attach(io: file, filename: 'Nurse Jackie', content_type: 'image/png')
 jackie.save!
-mildred = User.new(
-  first_name: "Mildred",
-  last_name: "RATCHED",
-  email: "mratched@mail.com",
+sabatier = User.new(
+  first_name: "J.Christophe",
+  last_name: "SABATIER",
+  email: "sabatier@mail.com",
   password: "nurseplan",
-  address: "rue du Vélodrome 33200 Bordeaux",
+  address: "107 Cours Balguerie Stuttenberg, 33300 Bordeaux",
   team: parc_bordelais)
-file = URI.open('https://tetu.com/wp-content/uploads/2020/09/ratched-1-1280x720.jpg')
-mildred.avatar.attach(io: file, filename: 'Mildred RATCHED', content_type: 'image/png')
-mildred.save!
+file = URI.open('https://avatars3.githubusercontent.com/u/70380868?v=4')
+sabatier.avatar.attach(io: file, filename: 'Jean-Christophe SABATIER', content_type: 'image/png')
+sabatier.save!
  #--------------------------------------------------------------------
 puts "SEED > ADD PATIENTS"
 alain = Patient.create!(
   first_name: "Alain",
-  last_name: "ALAIN",
+  last_name: "DUPOND",
   address: "34 rue du Parc 33200 Bordeaux ",
   compl_address: "Appeler avant de sonner",
-  phone: "+33666666666",
+  phone: "06 65 29 74 24",
   team: parc_bordelais)
 pierre = Patient.create!(
   first_name: "Pierre",
-  last_name: "PIERRE",
+  last_name: "DURAND",
   address: "12 rue Falquet 33200 Bordeaux ",
   compl_address: "Chien méchant",
-  phone: "06 66 66 66 66",
+  phone: "07 98 37 72 37",
   team: parc_bordelais)
 nicolas = Patient.create!(
   first_name: "Nicolas",
-  last_name: "NICOLAS",
+  last_name: "MARTIN",
   address: "3 rue Pasteur 33200 Bordeaux ",
-  compl_address: "",
-  phone: "0666666666",
+  compl_address: "Passer par le gardien",
+  phone: "05 34 97 27 63",
   team: parc_bordelais)
 hugues = Patient.create!(
   first_name: "Hugues",
-  last_name: "HUGUES",
+  last_name: "CAPES",
   address: "401 avenue de la Libération 33200 Bordeaux",
-  compl_address: "",
-  phone: "06.66.66.66.66",
+  compl_address: "Sonner au 403",
+  phone: "06 04 28 93 23",
   team: parc_bordelais)
 jacques = Patient.create!(
   first_name: "Jacques ",
-  last_name: "JACQUES",
+  last_name: "MESRIN",
   address: "172 rue de l'École Normale 33200 Bordeaux",
   compl_address: "",
-  phone: "06.66.66.66.66",
+  phone: "06 66 99 24 21",
   team: parc_bordelais)
 fernand = Patient.create!(
   first_name: "Fernand ",
-  last_name: "FERNAND",
+  last_name: "DELL",
   address: "158 rue Jules Ferry 33200 Bordeaux",
   compl_address: "",
-  phone: "06 666 666 66",
+  phone: "07 34 93 23 46",
+  team: parc_bordelais)
+roger = Patient.create!(
+  first_name: "Roger ",
+  last_name: "NAIN",
+  address: "198 rue Jules Ferry 33200 Bordeaux",
+  compl_address: "Code porte 3572A",
+  phone: "07 00 93 23 67",
+  team: parc_bordelais)
+monique = Patient.create!(
+  first_name: "Monique ",
+  last_name: "RANOUX",
+  address: "135 rue Jules Ferry 33200 Bordeaux",
+  compl_address: "Sonner à DURAND",
+  phone: "06 34 12 23 46",
   team: parc_bordelais)
 #--------------------------------------------------------------------
 puts "SEED > ADD VISITES"
 patients = Patient.all
-delays = (-2..-1).to_a
+delays = (-5..-1).to_a
 delays.each do |day|
   position = 0
+  wish_time = 5
   (8..16).to_a.sample.times do
     Visit.create!(
       date: Date.today + day,
-      position: position +=1 ,
+      position: position +=1,
       time: nil,
-      wish_time: (6..20).to_a.sample,
+      wish_time: wish_time += 1,
       user: jackie,
       patient: patients.sample,
       is_done: true)
@@ -91,13 +106,14 @@ patients = Patient.all
 delays = (1..5).to_a
 delays.each do |day|
   position = 0
+  wish_time = 5
   (8..16).to_a.sample.times do
     Visit.create!(
       date: Date.today + day,
       position: position +=1 ,
       time: nil,
-      wish_time: (6..20).to_a.sample,
-      user: jackie,
+      wish_time: wish_time += 1,
+      user: sabatier,
       patient: patients.sample,
       is_done: false)
   end
@@ -109,31 +125,31 @@ Visit.create!(
   position: position += 1,
   time: nil,
   wish_time: 8,
-  user: jackie,
-  patient: alain,
+  user: sabatier,
+  patient: fernand,
   is_done: false)
 Visit.create!(
   date: Date.today,
   position: position += 1,
   time: nil,
   wish_time: 8,
-  user: jackie,
-  patient: pierre,
+  user: sabatier,
+  patient: roger,
   is_done: false)
 Visit.create!(
   date: Date.today,
   position: position += 1,
   time: nil,
   wish_time: 9,
-  user: jackie,
-  patient: nicolas,
+  user: sabatier,
+  patient: monique,
   is_done: false)
 Visit.create!(
   date: Date.today,
   position: position += 1,
   time: nil,
   wish_time: 9,
-  user: jackie,
+  user: sabatier,
   patient: hugues,
   is_done: false)
 Visit.create!(
@@ -141,7 +157,7 @@ Visit.create!(
   position: position += 1,
   time: nil,
   wish_time: 10,
-  user: jackie,
+  user: sabatier,
   patient: jacques,
   is_done: false)
 Visit.create!(
@@ -149,56 +165,56 @@ Visit.create!(
   position: position += 1,
   time: nil,
   wish_time: 10,
-  user: jackie,
+  user: sabatier,
   patient: fernand,
   is_done: false)
 Visit.create!(
   date: Date.today,
   position: position += 1,
   time: nil,
-  wish_time: 17,
-  user: jackie,
+  wish_time: 11,
+  user: sabatier,
   patient: alain,
   is_done: false)
 Visit.create!(
   date: Date.today,
   position: position += 1,
   time: nil,
-  wish_time: 17,
-  user: jackie,
+  wish_time: 11,
+  user: sabatier,
   patient: pierre,
   is_done: false)
 Visit.create!(
   date: Date.today,
   position: position += 1,
   time: nil,
-  wish_time: 18,
-  user: jackie,
+  wish_time: 12,
+  user: sabatier,
   patient: nicolas,
   is_done: false)
 Visit.create!(
   date: Date.today,
   position: position += 1,
   time: nil,
-  wish_time: 18,
-  user: jackie,
+  wish_time: 12,
+  user: sabatier,
   patient: hugues,
   is_done: false)
 Visit.create!(
   date: Date.today,
   position: position += 1,
   time: nil,
-  wish_time: 19,
-  user: jackie,
+  wish_time: 14,
+  user: sabatier,
   patient: jacques,
   is_done: false)
 Visit.create!(
   date: Date.today,
   position: position += 1,
   time: nil,
-  wish_time: 19,
-  user: jackie,
-  patient: fernand,
+  wish_time: 14,
+  user: sabatier,
+  patient: monique,
   is_done: false)
 #--------------------------------------------------------------------
 # Injection, Prise de sang, perfusion, alimentation gastro, CHimio
@@ -263,6 +279,7 @@ cr = [
   "Le service de dépôt de repas n'etait pas passé, j'ai dû prévenir la famille. Alerte glycémique",
   "La cicatrice de l'abdominoplastie est douloureuse pour le patient. A surveiller.",
 ]
+visits = Visit.where(is_done: true)
 visits.each do |visit|
   Minute.create!(
     visit: visit,
