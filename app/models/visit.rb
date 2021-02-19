a83409f6e02f444ad56973addba7c857e45783e4
JeanChristophe Sabatier <sabatier.jeanchristophe@gmail.com>
2021-02-19 11:00:25 +0100

    modifs sur retours de flo

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -4,7 +4,7 @@ class Visit < ApplicationRecord
   has_many :visit_cares, dependent: :destroy
   has_many :cares, through: :visit_cares
   has_many :minutes, dependent: :destroy
-  has_many :patients, through: :prescriptions
+  has_one :patients, through: :prescriptions
   validates :date, presence: true
   validates :position, presence: true, numericality: { only_integer: true, greater_or_equal_than: 0 }
   validates :wish_time, presence: true, inclusion: { in: (0..23).to_a }
