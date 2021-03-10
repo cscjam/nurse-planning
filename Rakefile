2c5c947d2d4bee1b930fe2a45354279cd908f091
JeanChristophe Sabatier <sabatier.jeanchristophe@gmail.com>
2021-03-10 18:17:58 +0100

    ImportUserCSV stuck on File Import Launch - 3hours

--- Rakefile
+++ Rakefile
@@ -4,3 +4,18 @@
 require_relative 'config/application'
 
 Rails.application.load_tasks
+
+namespace :import do
+  desc 'an import profiles test'
+  task :patients => :environment do
+    my_file = "test/Profilestest.CSV"
+    import = ImportUserCSV.new(file: my_file)
+    import.valid_header?
+    import.report.message
+
+    import.run!
+    import.run.success?
+    import.report.message
+  end
+end
+
--- app/models/import_user_csv.rb
+++ app/models/import_user_csv.rb
@@ -6,12 +6,11 @@ class ImportUserCSV < ApplicationRecord
   column :team_id, as: [ /team.?id/i, "cabinet"], required: true
   column :first_name, as: [ /first.?name/i, /pr(é|e)nom/i ], required: true
   column :last_name,  as: [ /last.?name/i, "nom" ] , required: true
-  column :address, as: [ /a.?ddress/i, "adresse"], to: ->(adress) {adress.downcase}
   column :phone, as: [ /p.?hone/i, /t(é|e)phone/i]
+  column :address, as: [ /a.?ddress/i, "adresse"], to: ->(adress) {adress.downcase}
   column :compl_address, as: [ /compl.?address/i, "complement adresse" ]
 
-  identifier :last_name, :address #will check if patient already exist
+  identifier :last_name, :phone #will check if patient already exist
 
   when_invalid :skip #or :abort when invalid entry
 end
-
